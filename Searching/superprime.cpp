8ad5e3f73e6776370c3a115b1fcbec622b813ca1
Tanuj Rohilla <tanujrohilla325@gmail.com>
2019-06-23 23:22:01 +0530

    Update superprime.cpp

--- Searching/superprime.cpp
+++ Searching/superprime.cpp
@@ -8,26 +8,22 @@ bitset<10000007> prime;
 vector<ll> s;
 
 int main()
- {
+{
     ll t;
     cin>>t;
     prime[0]=1;
     prime[1]=1;
-   	for(ll i=2;i*i<=10000007;i++)
-   	{
-       	if(prime[i]==0)
-       	{
+   	for(ll i=2;i*i<=10000007;i++){
+       	if(prime[i]==0){
         	for(ll j=i*2;j<=10000007;j+=i)
                prime[j]=1;
        	}
    	}
-   	for(ll i=2;i<=10000007;i++)
-   	{
+   	for(ll i=2;i<=10000007;i++){
        	if(prime[i]==0 && prime[i-2]==0)
-       		s.push_back(i);
+       		 s.push_back(i);
    	}
-    while(t--)
-    {
+    while(t--){
          ll n;
          cin>>n;
          ll ans=upper_bound(s.begin(),s.end(),n)-s.begin();
