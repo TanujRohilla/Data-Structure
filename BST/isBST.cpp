0361c331bcd3585c1a0212887e92cd8cca8f63c6
TanujRohilla <tanujrohilla325@gmail.com>
2019-07-19 01:39:23 +0530

    is BST

---
+++ BST/isBST.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* right;
+    Node* left;
+    
+    Node(int x){
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+int isBSTUtil(Node* root, int min, int max){
+    if(root==NULL)
+        return 1;
+    if(root->data<min  || root->data>max)
+        return 0;
+    return isBSTUtil(root->left,min,root->data-1) && isBSTUtil(root->right,root->data+1,max);
+}
+
+bool isBST(Node* root) {
+    return isBSTUtil(root,INT_MIN,INT_MAX);
+}
+
+int main()
+{
+  int t;
+  struct Node *child;
+  scanf("%d", &t);
+  while (t--)
+  {
+     map<int, Node*> m;
+     int n;
+     scanf("%d",&n);
+     struct Node *root = NULL;
+     while (n--)
+     {
+        Node *parent;
+        char lr;
+        int n1, n2;
+        scanf("%d %d %c", &n1, &n2, &lr);
+        if (m.find(n1) == m.end())
+        {
+           parent = new Node(n1);
+           m[n1] = parent;
+           if (root == NULL)
+             root = parent;
+        }
+        else
+           parent = m[n1];
+        child = new Node(n2);
+        if (lr == 'L')
+          parent->left = child;
+        else
+          parent->right = child;
+        m[n2]  = child;
+     }
+     cout << isBST(root) << endl;
+  }
+  return 0;
+}
+
+
+
